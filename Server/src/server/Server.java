b22e30ccbcd86541014517a0652dd098e9ad4f6f
Lucas Guevremont <lucasg25@icloud.com>
2025-01-29 18:26:49 -0500

    feature(closing Server):
    server can now be close with the "/quit" command

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -7,12 +7,14 @@ import java.util.regex.Pattern;
 
 public class Server {
 	
-	private static ServerSocket Listener;
+	private static ServerSocket serverSocket;
 	private static final String IP_REGEX = 
 	        "^((25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)$";
 	static Scanner scanner = new Scanner(System.in);
 	static String ipAddress;
 	static int port;
+	private static boolean running = true;
+	
 	
 	public static void main (String[] args){
 		serverLaunch();
@@ -36,16 +38,42 @@ public class Server {
 		}
 		
 		try {
-			Listener = new ServerSocket();
-			Listener.setReuseAddress(true);
+			serverSocket = new ServerSocket();
+			serverSocket.setReuseAddress(true);
 			InetAddress serverIp = InetAddress.getByName(ipAddress);
-			Listener.bind(new InetSocketAddress(serverIp, port));
+			serverSocket.bind(new InetSocketAddress(serverIp, port));
 			System.out.format("Le serveur roule sur l'addresse IP %s et le port %d", ipAddress, port);
-	}
-		catch (IOException e) {
-			 System.out.println("Erreur lors du démarrage du serveur.");
+			running = true;
 		}
+		catch (IOException e) {
+			 System.out.println("Erreur lors du démarrage du serveur: " + e.getMessage());
+		}
+		
+		new Thread(() -> 
+		{
+			while(true) {
+				if (scanner.nextLine().equalsIgnoreCase("/quit")) {
+					closeServer();
+					break;
+				}
+			}
+		}).start();
 	
 	}
+	
+	public static void closeServer() {
+		running = false;
+		
+		try{
+			if (serverSocket != null) serverSocket.close();
+			System.out.println("Serveur arrêté.");
+		}
+		catch (IOException e) {
+			System.out.println("Erreur lors de l'arrêt du serveur: " + e.getMessage());
+		}
+		
+	}
+	
+	
 	
 }
